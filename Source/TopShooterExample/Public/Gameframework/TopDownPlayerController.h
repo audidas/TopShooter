63dd18475cd20ddaa1817bfd648f9e1ddac37933
audidas <audidas@naver.com>
2026-01-16 10:01:08 +0900

    Make Controller TopDownPlayer

---
+++ Source/TopShooterExample/Public/Gameframework/TopDownPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TopDownPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPSHOOTEREXAMPLE_API ATopDownPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
